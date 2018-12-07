93c7673f429299b397a2402f64bd7496ebc56469
Guest User <Guest@epicodus-25.local>
2018-12-07 12:19:54 -0800

    add Sounds component

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Sounds  from './Sounds';
+
+
+function App(){
+  return (
+    <div>
+        <h1>CapstoneProjectPlanning</h1>
+       <Sounds />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Sounds.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Sounds() {
+    return ( 
+        <h1>Play</h1>
+     );
+}
+ 
+export default Sounds;
